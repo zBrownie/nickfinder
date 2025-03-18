85851a719cb1b663fbf7f565143e35a776c306c7
bzenerato <bru.nomoraes@hotmail.com>
2025-03-18 01:47:11 -0300

    feat: github pages repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/nickfinder/",
   plugins: [
     react(),
     tailwindcss(),
